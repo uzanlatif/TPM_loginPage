28910613f752f5d3fc50c931d4eb4eb758b0ffac
uzan <uzanlatif@gmail.com>
2022-02-24 11:10:07 +0700

    pretty

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,15 @@ import 'login_page.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: "Uzan App",
       theme: ThemeData(primarySwatch: Colors.pink),
       home: const LoginPage(),
-
     );
   }
 }
